ad141e535f6728bd2c79ffd13b92f44e03f05921
Igor <igormgg@hotmail.com>
2021-10-27 11:29:54 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./reset.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Login + Refatorando Provider</h1>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
